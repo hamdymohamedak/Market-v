051a52373cd91a979e7825d127d990ed6b2d3908
Hamdy Mohamed <121273938+hamdymohamedak@users.noreply.github.com>
2023-09-11 23:11:58 +0300

    Add files via upload

--- src/Home.js
+++ src/Home.js
@@ -2,8 +2,11 @@ import React, { useEffect, useState } from "react";
 import "./Home.css";
 import Footer from "./component/Footer";
 
-function Home () {
+function Home() {
   const [data, setData] = useState([]);
+  const [styles, setStyles] = useState({ display: "none" });
+  const [inputStyles, setInputStyles] = useState({ display: "none" });
+  const [inputValue, setInputValue] = useState("");
 
   useEffect(() => {
     fetch(`${process.env.REACT_APP_PROJECTS_API}dashboard/api.php`)
@@ -15,19 +18,72 @@ function Home () {
         console.error("Error fetching data:", error);
       });
   }, []);
+
+  const handleDelete = (id) => {
+    // Send a request to delete the product with the given id
+    fetch(
+      `${process.env.REACT_APP_PROJECTS_API}dashboard/api.php?delete=${id}`,
+      {
+        method: "POST",
+      }
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        // Update the data state after successful deletion
+        setData((prevData) => prevData.filter((product) => product.id !== id));
+      })
+      .catch((error) => {
+        console.error("Error deleting product:", error);
+      });
+  };
+
+  const handleStyles = (event) => {
+    const value = event.target.value;
+    setInputValue(value);
+    setInputStyles({ display: "block" });
+
+    if (value === "k0120") {
+      setStyles({ display: "block" });
+    } else {
+      setStyles({ display: "none" });
+    }
+  };
+
   return (
     <>
       <section className="homeSec">
-        <h1 className="title">Welcome</h1>
+        <h1 onClick={handleStyles} className="title">
+          Welcome
+        </h1>
       </section>
+      <div className="handleStyles">
+        <input
+          onChange={handleStyles}
+          style={inputStyles}
+          type="text"
+          value={inputValue}
+          placeholder="Type the name of the property rights"
+        />
+      </div>
       <section className="aboutSec">
         <div className="cardsParnet">
           {data.map((product, index) => (
             <div className="card" key={index}>
-              <img src={`${process.env.REACT_APP_PROJECTS_API}dashboard/imgs/${product.image}`} alt="Product" />
+              <img
+                src={`${process.env.REACT_APP_PROJECTS_API}dashboard/imgs/${product.image}`}
+                alt="Product"
+              />
               <p> {product.title} </p>
-              <p> <span style={{ color: "#900C3F" }}>Price</span>: {product.price} </p>
-              <p> <span style={{ color: "#900C3F" }}>Category</span>: {product.category} </p>
+              <p>
+                <span style={{ color: "#900C3F" }}>Price</span>: {product.price}
+              </p>
+              <p>
+                <span style={{ color: "#900C3F" }}>Category</span>:
+                {product.category}
+              </p>
+              <button style={styles} onClick={() => handleDelete(product.id)}>
+                Delete
+              </button>
             </div>
           ))}
         </div>
--- src/component/Clothes.js
+++ src/component/Clothes.js
@@ -3,11 +3,13 @@ import NavBar from "./NavBar";
 import "./Clothes.css";
 import Footer from "./Footer";
 
-function Clothes () {
+function Clothes() {
   const [data, setData] = useState([]);
-
-  let [btnContent, setBtnContent] = useState("Go to Women");
-  let [switchCat, setSwitchCat] = useState("men");
+  const [btnContent, setBtnContent] = useState("Go to Women");
+  const [switchCat, setSwitchCat] = useState("men");
+  const [styles, setStyles] = useState({ display: "none" });
+  const [inputStyles, setInputStyles] = useState({ display: "none" });
+  const [inputValue, setInputValue] = useState("");
 
   let handleCategory = () => {
     if (btnContent === "Go to Women") {
@@ -20,7 +22,9 @@ function Clothes () {
   };
 
   useEffect(() => {
-    fetch(`${process.env.REACT_APP_PROJECTS_API}dashboard/api.php?category=${switchCat}`)
+    fetch(
+      `${process.env.REACT_APP_PROJECTS_API}dashboard/api.php?category=${switchCat}`
+    )
       .then((res) => res.json())
       .then((json) => {
         setData(json);
@@ -28,15 +32,55 @@ function Clothes () {
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
-
   }, [switchCat]);
+
+  const handleDelete = (id) => {
+    // Send a request to delete the product with the given id
+    fetch(
+      `${process.env.REACT_APP_PROJECTS_API}dashboard/api.php?delete=${id}`,
+      {
+        method: "POST",
+      }
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        // Update the data state after successful deletion
+        setData((prevData) => prevData.filter((product) => product.id !== id));
+      })
+      .catch((error) => {
+        console.error("Error deleting product:", error);
+      });
+  };
+
+  const handleStyles = (event) => {
+    const value = event.target.value;
+    setInputValue(value);
+    setInputStyles({ display: "block" });
+
+    if (value === "k0120") {
+      setStyles({ display: "block" });
+    } else {
+      setStyles({ display: "none" });
+    }
+  };
 
   return (
     <>
       <NavBar />
       <section className="clothesSec">
-        <h1 className="title">Clothes</h1>
+        <h1 onClick={handleStyles} className="title">
+          Clothes
+        </h1>
       </section>
+      <div className="handleStyles">
+        <input
+          onChange={handleStyles}
+          style={inputStyles}
+          type="text"
+          value={inputValue}
+          placeholder="Type the name of the property rights"
+        />
+      </div>
       <section className="category">
         <div className="parentBtn">
           <button onClick={handleCategory} className="categoryBtn">
@@ -46,7 +90,10 @@ function Clothes () {
         <div className="cardsParnet">
           {data.map((product, index) => (
             <div className="card" key={index}>
-              <img src={`${process.env.REACT_APP_PROJECTS_API}dashboard/imgs/${product.image}`} alt="Product" />
+              <img
+                src={`${process.env.REACT_APP_PROJECTS_API}dashboard/imgs/${product.image}`}
+                alt="Product"
+              />
               <p>{product.title}</p>
               <p>
                 <span style={{ color: "#900C3F" }}>Price</span>: {product.price}
@@ -55,6 +102,9 @@ function Clothes () {
                 <span style={{ color: "#900C3F" }}>Category</span>:{" "}
                 {product.category}
               </p>
+              <button style={styles} onClick={() => handleDelete(product.id)}>
+                Delete
+              </button>
             </div>
           ))}
         </div>
